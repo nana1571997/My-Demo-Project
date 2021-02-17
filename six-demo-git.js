2c70ff2832e09ccdf126b749c54e55f5025d5b66
nana cong chua <leona.1571995@gmail.com>
2021-02-17 12:42:56 +0700

    add six-demo-test by branch

---
+++ six-demo-git.js
@@ -0,0 +1,18 @@
+const n = 5;
+function createArray(row) {
+    var arr = [];
+    for(let i = 0; i < row;i++){
+        arr[i] = new Array(row).fill("0");
+    }
+    return arr
+}
+
+const myArr = createArray(5)
+
+for(let i = 0; i < n; i ++){
+    for(let j = 0; j < n; j++){
+        myArr[i][i] = "*";
+        myArr[i][n-1-i]="*"
+    }
+}
+console.log(myArr)
