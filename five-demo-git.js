24b092c2e2d1cb1be87aaaa0715ef47b203d8427
nana cong chua <leona.1571995@gmail.com>
2021-02-17 12:55:17 +0700

    add something in five-demo-git

--- five-demo-git.js
+++ five-demo-git.js
@@ -2,8 +2,9 @@ const arr = [1,2,3,4,5,6,7]
 for (let i = 0; i <= arr.length; i++){
     console.log(i)
 }
-
+const justin="Biber"
 function helloWord(name){
     console.log("hello"+name)
 }
 helloWord(justin)
+helloWord("lyly")
